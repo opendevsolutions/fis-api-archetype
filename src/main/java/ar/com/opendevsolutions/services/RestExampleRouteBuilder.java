26a49854ca38e0370b4f2228651d6c644f28b4d0
jamadrigale <jm@opendevsolutions.com.ar>
2020-10-05 01:23:07 -0300

    Changes to be committed: 	modified:   src/main/java/ar/com/opendevsolutions/services/RestExampleRouteBuilder.java 	modified:   src/main/java/ar/com/opendevsolutions/utils/Constants.java 	modified:   src/main/resources/spring/camel-context.xml

--- src/main/java/ar/com/opendevsolutions/services/RestExampleRouteBuilder.java
+++ src/main/java/ar/com/opendevsolutions/services/RestExampleRouteBuilder.java
@@ -1,10 +1,6 @@
 package ar.com.opendevsolutions.services;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
-
-import ar.com.opendevsolutions.utils.Constants;
 
 @Component
 public class RestExampleRouteBuilder extends ExampleRouteBuilder {
@@ -13,19 +9,11 @@ public class RestExampleRouteBuilder extends ExampleRouteBuilder {
   public void addRoutes() throws Exception {
 
 	  from("direct:findAllEntityExample")
-		.setHeader(Exchange.HTTP_METHOD, constant(Constants.GET))	
-		.setHeader(Exchange.HTTP_PATH, constant(Constants.EMPTY))
-		.setHeader(Constants.ACCEPT, constant(Constants.APPLICATION_JSON))
-		.setHeader(Constants.CONTENT_TYPE, constant(Constants.APPLICATION_JSON))
-	  	.to("jetty:http://18.231.124.229:8080/client/?bridgeEndpoint=true")
+	  	.setBody().simple("[{}]")
 	  	.log("RestExample-FindAll: ${body}");
 	  
 	  from("direct:findEntityExampleById")
-		.setHeader(Exchange.HTTP_METHOD, constant(Constants.GET))
-		.setHeader(Exchange.HTTP_PATH, constant(Constants.EMPTY))
-		.setHeader(Constants.ACCEPT, constant(Constants.APPLICATION_JSON))
-		.setHeader(Constants.CONTENT_TYPE, constant(Constants.APPLICATION_JSON))
-		.to("jetty:http://18.231.124.229:8080/client/1?bridgeEndpoint=true")
+	  	.setBody().simple("{}")
 		.log("RestExample-FindById: ${body}");
   }
 
--- src/main/java/ar/com/opendevsolutions/utils/Constants.java
+++ src/main/java/ar/com/opendevsolutions/utils/Constants.java
@@ -7,14 +7,32 @@ public class Constants {
 	}
 	
 	public static final String AUTHORIZATION = "Authorization";
+	public static final String CONTAINER_ID = "containerId";
+	public static final String GET_VERSION_FROM_TASK = "direct:getVersionFromTask";
+	public static final String GET_VERSION_FROM_PROCESS = "direct:getVersionFromProcess";
+	public static final String MERGE_JSON_PROCESS_TASK = "direct:mergeJsonProcessTask";
 	public static final String ACCEPT = "Accept";
 	public static final String APPLICATION_JSON = "application/json";
 	public static final String CONTENT_TYPE = "Content-Type";
+	public static final String SERVICE_EXCEPTION = "direct:serviceException";
+	public static final String RHPAM_AUTHORIZATION = "{{rhpam.authorization}}";
+	public static final String ACTIVE_USER_TASKS = "active-user-tasks";
 	public static final String EMPTY = "";
+	public static final String TAREA_ID = "tareaId";
 	public static final String GET = "GET";
 	public static final String POST = "POST";
 	public static final String PUT = "PUT";
 	public static final String DELETE = "DELETE";
+	public static final String COND_COLUMN = "cond-column";
+	public static final String COND_OPERATOR = "cond-operator";
+	public static final String COND_VALUE = "cond-values";
+	public static final String COLUMN = "column";
+	public static final String OPERATOR = "operator";
+	public static final String PARAM = "param";
+	public static final String DIRECTION = "direction";
+	public static final String EQUALS_TO = "EQUALS_TO";
+	public static final String BETWEEN = "BETWEEN";
+	public static final String CLEAR_REST_HEADERS = "direct:clearRestHeaders";
 	
 	
 }
